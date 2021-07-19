72f0b88e9533fb5f3698755ab3a5ce003a5bea6b
Chintan Karki <karkichintan2000@gmail.com>
2021-07-19 22:55:50 +0545

    Login Form handling Password

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,19 +1,55 @@
 import React, { Component } from "react";
 
 class LoginForm extends Component {
+  state = {
+    account: {
+      username: "",
+      password: "",
+    },
+  };
+
+  //   handleSubmit = (e) => {
+  //     e.preventDefault();
+  //     const a = this.username.current.value;
+  //     console.log("Submitted", a);
+  //   };
+
+  handleChange = ({ currentTarget: input }) => {
+    const account = { ...this.state.account };
+    account[input.name] = input.value;
+    this.setState({ account });
+  };
+
   render() {
+    const { account } = this.state;
     return (
       <div>
         <h1>Login</h1>
         <form onSubmit={this.handleSubmit}>
           <div className="form-group">
             <label htmlFor="username">Username</label>
-            <input id="username" type="text" className="form-control" />
+            <input
+              autoFocus
+              name="username"
+              id="username"
+              type="text"
+              className="form-control"
+              value={account.username}
+              onChange={this.handleChange}
+            />
           </div>
           <div className="form-group">
             <label htmlFor="password">Password</label>
-            <input id="password" type="text" className="form-control" />
+            <input
+              id="password"
+              name="password"
+              type="text"
+              className="form-control"
+              value={account.password}
+              onChange={this.handleChange}
+            />
           </div>
+
           <button className="btn btn-primary">LOGIN</button>
         </form>
       </div>
