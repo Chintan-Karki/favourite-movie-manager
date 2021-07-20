7b8cddd52717c1060e6dc99d17d6bae239ec8d46
Chintan Karki <karkichintan2000@gmail.com>
2021-07-20 16:24:59 +0545

    Live validation added

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -28,10 +28,28 @@ class LoginForm extends Component {
     if (errors) return;
   };
 
+  validateProperty = ({ name, value }) => {
+    if (name === "username") {
+      if (value.trim() === "") {
+        return "Username is required";
+      }
+    }
+    if (name === "password") {
+      if (value.trim() === "") {
+        return "Password is required";
+      }
+    }
+  };
+
   handleChange = ({ currentTarget: input }) => {
+    const errors = { ...this.state.errors };
+    const errorMessage = this.validateProperty(input);
+    if (errorMessage) errors[input.name] = errorMessage;
+    else delete errors[input.name];
+
     const account = { ...this.state.account };
     account[input.name] = input.value;
-    this.setState({ account });
+    this.setState({ account, errors });
   };
 
   render() {
